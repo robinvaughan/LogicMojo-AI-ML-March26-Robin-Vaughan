952e80a92f272562837ed9837605367eb81c3d59
robinvaughan <robin.vaughan@gmail.com>
2026-03-18 00:06:43 +0530

    Fifth Assignment.

--- class_04_march_15_26_data_structure/fifth-assignment-15-march-2026.py
+++ class_04_march_15_26_data_structure/fifth-assignment-15-march-2026.py
@@ -17,3 +17,13 @@
     # Product_Name → product purchased 
     # Price → price of the product 
     # Status → order status (Completed, Returned, Fraud)
+
+
+headers = ["Order ID" , "Product Name" , "Price" , "Status"]
+orders = [ ["ORD001","Laptop",75000,"Completed"], ["ORD002","Mouse",500,"Completed"], ["ORD003","Laptop",75000,"Returned"], ["ORD004","Keyboard",1500,"Completed"], ["ORD005","Laptop",75000,"Completed"], ["ORD006","Monitor",20000,"Fraud"], ["ORD007","Mouse",500,"Completed"], ["ORD008","Laptop",75000,"Completed"] ]
+print("~" * 60) # Separator line
+print(f"{headers[0]:<15}{headers[1]:<15}{headers[2]:<15}{headers[3]:<15}")
+print("~" * 60) # Separator line
+for index, order in enumerate(orders):
+    print(f"{order[0]:<15}{order[1]:<15}{order[2]:<15}{order[3]:<15}")
+    print("-" * 60) # Separator line
