cc43fd54becef1e0b0c7b1e1417498624a594c9e
robinvaughan <robin.vaughan@gmail.com>
2026-03-17 22:10:49 +0530

    Assignment 1 Completed.

--- class_04_march_15_26_data_structure/assignments-15-march-2026.py
+++ class_04_march_15_26_data_structure/assignments-15-march-2026.py
@@ -13,7 +13,7 @@
     # Calculate the total spending for the day.
     # Create a new list containing only unique transaction values.
     # Sort the unique transaction list in descending order to view the largest transactions first.
-    
+
 transactions = [1200, 3000, 250, 7800, 540, 3000, 9100, 250, 1500, 7800]
 
 # Identify and display all transaction values that appear more than once.
@@ -24,3 +24,38 @@ for transaction in transactions:
         if (transaction in test_list_one):
             print(f"The Frequency of transaction -'{transaction}' is - '{frequency}'")
     test_list_one.append(transaction)
+
+
+
+# Detect transactions greater than ₹5000 as these may require fraud monitoring.
+for index, transaction in enumerate(transactions):
+    if (transaction > 5000):
+            print(f"The transaction -'{transaction}' at Position ID - {index} might be a suspected transaction!!!")
+
+
+
+# Calculate the total spending for the day.
+total_spending = 0
+for transaction in transactions:
+    total_spending += transaction
+print(f"Total spending for the day - {total_spending}")
+
+
+
+# Create a new list containing only unique transaction values.
+test_list_two = list()
+for transaction in transactions:
+    if (transaction not in test_list_two):
+        test_list_two.append(transaction)
+print(f"The new list with ONLY Unique transaction values is - {test_list_two}")
+
+
+
+ # Sort the unique transaction list in descending order to view the largest transactions first.
+test_list_three= list()
+for transaction in transactions:
+    if (transaction not in test_list_three):
+        test_list_three.append(transaction)
+test_list_three.sort(reverse=True)
+print(f"The Descending Order for the  unique transaction list - {test_list_three}")  
+print(f"Largest transactions first - {test_list_three[0]}")        
